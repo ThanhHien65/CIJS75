da27891f4d5a9f23b8557e201cb68c614c6fa98a
Orther <hien@hanbiro.vn>
2022-06-27 09:03:12 +0700

    done

---
+++ REACTJS-Football/my_app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Football} from './component/Football/Football';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Football></Football>
+    </div>
+  );
+}
+
+export default App;
