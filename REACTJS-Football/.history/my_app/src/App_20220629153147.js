04d6a429af253af693cf0a40744d2fa713aa22f6
thanhien65 <hiennguyenthanh651@gmail.com>
2022-07-01 01:48:00 +0700

    done

---
+++ REACTJS-Football/.history/my_app/src/App_20220629153147.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Football } from "./component/Football/Football";
+
+function App() {
+  return (
+    <div className="App">
+      <Football></Football>
+    </div>
+  );
+}
+
+export default App;
